ffa7a5181bdaaddecfc30ebe8f63769d69a58ae4
MOHAMMAD NADEEM <mnadeem.mn838@gmail.com>
2022-11-15 12:04:37 +0530

    Updates

---
+++ precache-manifest.7a649be1dacb8b022ee939b8f40ff224.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec27a07d4385a1352ffc",
+    "url": "/utilitytool/static/css/main.137d7722.chunk.css"
+  },
+  {
+    "revision": "ec27a07d4385a1352ffc",
+    "url": "/utilitytool/static/js/main.ec27a07d.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/utilitytool/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "883d65ef5b6f31fc39bb",
+    "url": "/utilitytool/static/js/2.883d65ef.chunk.js"
+  },
+  {
+    "revision": "027a889a482c0f998808",
+    "url": "/utilitytool/static/js/runtime~main.027a889a.js"
+  },
+  {
+    "revision": "0de1338ce13bfa146d4f893d76856291",
+    "url": "/utilitytool/index.html"
+  }
+];
